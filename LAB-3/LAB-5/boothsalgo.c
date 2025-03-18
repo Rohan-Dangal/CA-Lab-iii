52aa091e22975f1e5e588b0616dc7733b6959cfd
Rohan Dangal <johndangal2061@gmail.com>
2025-03-18 07:32:35 +0545

    booths algorithm

---
+++ LAB-3/LAB-5/boothsalgo.c
@@ -0,0 +1,172 @@
+#include <stdio.h>
+
+#define maxsize 4
+
+void dectobin(int arr[], int dec);
+void arithmeticright(int arr1[], int arr2[], int *qo);
+void putval(int arr[]);
+void increment(int arr[]);
+void putcal(int count, int A[], int Q[], int qo, int M[], int mcomp[], int num);
+int updateqo(int arr[]);
+
+int main()
+{
+    int multiplicand, multiplier;
+
+    printf("Enter the multiplier Q<16: ");
+    scanf("%d", &multiplier);
+    printf("Enter the multiplicand M<16: ");
+    scanf("%d", &multiplicand);
+
+    int num = (multiplicand > 0) ? 1 : 0;
+    if (num == 0)
+    {
+        multiplicand *= -1;
+    }
+    int qo = 0;
+
+    int M[maxsize] = {0};
+    int A[maxsize] = {0};
+    int Q[maxsize] = {0};
+
+    dectobin(Q, multiplier);
+    dectobin(M, multiplicand);
+
+    int mcomp[maxsize];
+    if (num == 0)
+    {
+        for (int i = maxsize - 1; i >= 0; i--)
+        {
+            mcomp[i] = M[i] == 1 ? 0 : 1;
+        }
+        increment(mcomp);
+    }
+
+    printf("C\tA\tQ\tq-\tM\n");
+
+    for (int count = maxsize; count >= 0; count--)
+    {
+        putcal(count, A, Q, qo, M, mcomp, num);
+        if (count == 0)
+        {
+            break;
+        }
+        if (qo == Q[maxsize - 1])
+        {
+            arithmeticright(A, Q, &qo);
+            printf("\tshift\n");
+        }
+        else if (qo == 1 && (Q[maxsize - 1] == 0))
+        {
+            int carry = 0;
+            for (int i = maxsize - 1; i >= 0; i--)
+            {
+                int temp = A[i] + mcomp[i] + carry;
+                A[i] = temp % 2;
+                carry = temp / 2;
+            }
+            putcal(888, A, Q, qo, M, mcomp, num);
+            printf("\tshift\n");
+            arithmeticright(A, Q, &qo);
+        }
+        else if (qo == 0 && Q[maxsize - 1] == 1)
+        {
+            int carry = 0;
+            for (int i = maxsize - 1; i >= 0; i--)
+            {
+                int temp = A[i] + M[i] + carry;
+                A[i] = temp % 2;
+                carry = temp / 2;
+            }
+            putcal(999, A, Q, qo, M, mcomp, num);
+            printf("\tshift\n");
+            arithmeticright(A, Q, &qo);
+        }
+    }
+    printf("\nFinal result is:");
+    if (num == 0)
+    {
+        printf("-");
+    }
+    putval(A);
+    putval(Q);
+    return 0;
+}
+
+void dectobin(int arr[], int dec)
+{
+    int i = maxsize - 1;
+    while (dec != 0 && i >= 0)
+    {
+        arr[i] = dec % 2;
+        dec /= 2;
+        i--;
+    }
+    while (i >= 0)
+    {
+        arr[i] = 0;
+        i--;
+    }
+}
+
+void increment(int arr[])
+{
+    int carry = 1;
+    for (int i = maxsize - 1; i >= 0; i--)
+    {
+        int temp = arr[i] + carry;
+        arr[i] = temp % 2;
+        carry = temp / 2;
+    }
+}
+
+void arithmeticright(int arr1[], int arr2[], int *qo)
+{
+    int temp = arr1[0];
+    *qo = arr2[maxsize - 1];
+    for (int i = maxsize - 1; i > 0; i--)
+    {
+        arr2[i] = arr2[i - 1];
+    }
+    arr2[0] = arr1[maxsize - 1];
+    for (int i = maxsize - 1; i > 0; i--)
+    {
+        arr1[i] = arr1[i - 1];
+    }
+    arr1[0] = temp;
+}
+
+void putval(int arr[])
+{
+    for (int i = 0; i < maxsize; i++)
+    {
+        printf("%d", arr[i]);
+    }
+}
+
+void putcal(int count, int A[], int Q[], int qo, int M[], int mcomp[], int num)
+{
+    if (count == 999)
+    {
+        printf("\nA-M\t");
+    }
+    else if (count == 888)
+    {
+        printf("\nA+M\t");
+    }
+    else
+    {
+        printf("%d\t", count);
+    }
+    putval(A);
+    printf("\t");
+    putval(Q);
+    printf("\t%d\t", qo);
+    putval(num > 0 ? M : mcomp);
+    printf("\t");
+}
+
+int updateqo(int arr[])
+{
+    return arr[maxsize - 1];
+}
